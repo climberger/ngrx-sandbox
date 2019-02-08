602eb5b73f8410812b7e7d3dd12773730db1037b
Carsten Limberger <carsten.limberger@nterra.com>
2019-02-08 13:22:04 +0100

    Add TODOs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// TODO: Add meta reducers
+// TODO: Add normal reducers for app level (router reducer)
+// TODO: Add effects on app level
+// TODO: Create shared module
+// TODO: Add NgrxFormsModule
+// TODO: Add store dev tools
+// TODO: Add router serializer
+
 @NgModule({
   declarations: [
     AppComponent
